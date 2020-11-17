8d05fb9dd05afe53e215f5349ebfbec16e4019fe
Ali Soltani <soltani1384@gmail.com>
2020-11-17 12:20:03 +0330

    adding packages needed for rabbitmq as well as masstransit

---
+++ src/Services/Marketing/Marketing.API/Infrastructure/Swagger/SwaggerServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Marketing.API.Infrastructure.Swagger
+{
+    public static class SwaggerServiceCollectionExtensions
+    {
+		public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            // Register the Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Marketing.API",
+                    Version = "v1",
+                    Description = "The Portal Marketing Service API",
+                });
+            });
+
+            return services;
+        }
+	}
+}
